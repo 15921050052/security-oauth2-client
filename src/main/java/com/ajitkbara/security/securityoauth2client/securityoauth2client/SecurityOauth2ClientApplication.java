d9c9e6b5f3623dd482c1006f35d833a21404529d
Ajit Baral <ajitkbaralofficial@gmail.com>
2017-10-26 19:58:32 +0545

    Initial commit

---
+++ src/main/java/com/ajitkbara/security/securityoauth2client/securityoauth2client/SecurityOauth2ClientApplication.java
@@ -0,0 +1,12 @@
+package com.ajitkbara.security.securityoauth2client.securityoauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauth2ClientApplication.class, args);
+	}
+}
